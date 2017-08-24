e89d841be978ebf6c26cb72d1d5653fd50e936c7
Volodumur Boychuk <rilhad1@gmail.com>
2017-08-24 12:26:04 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -1,38 +1,52 @@
 let n = parseInt(prompt('Enter N'));
 
-function fib(n){
-  if ( n == 0 ) {
+function print(data) {
+  document.write(data + '<br>');
+}
+
+function fib(n) {
+  if (n == 0) {
     return 0;
-  }else if (n == 1) {
+  }
+  else if (n == 1) {
     return 1;
   }
-  return fib(n-2) + fib(n-1);
+  return fib(n - 2) + fib(n - 1);
 }
-document.write(fib(n));
 
-// function bine(n){
-//   let index = Math.pow(5, 0.5);
- 
-//   let left = (1 + index) / 2;
-//   let right = (1 - index) / 2;
-  
-//   return Math.round((Math.pow(left, n) - Math.pow(right, n)) / index);
-// }
+function bine(n) {
+  let sqrt5 = Math.sqrt(5);
+  let a = (1 + sqrt5) / 2;
+  let b = (1 - sqrt5) / 2;
+  return Math.round((Math.pow(a, n) - Math.pow(b, n)) / sqrt5);
+}
 
-// function fib2(n)
-// {
-//   if (n <= 2) return 1;
-//   let x = 1;
-//   let y = 1;
-//   let res = 0;
-//   for (var i = 2; i < n; i++)
-//   {
-//     res = x + y;
-//     x = y;
-//     y = res;
-//   }
-//   return res;
-// }
+function fib2(n) {
+  if (n <= 2) {
+    return 1;
+  } else {
+    let a = 1; 
+    let b = 1; 
+    let res = 0;
+    for (let i = 2; i < n; i++) {
+      res = a + b;
+      a = b;
+      b = res;
+    }
+  return res;
+  }
+}
 
-// document.write(bine(n));
-// document.write(fib2(n));
+function arr(n) {
+  let fib3 = [1, 1];
+  for (let i = 2; i < n; i++) {
+    fib3[i] = fib3[i - 1] + fib3[i - 2];
+  }
+  // return fib3[n];
+  return fib3[fib3.length - 1];
+}
+
+print('Рекурсія: ' + fib(n));
+print('Формула Біне: ' + bine(n));
+print('Цикл: ' + fib2(n));
+print('Цикл+масив: ' + arr(n) + '<br>');
